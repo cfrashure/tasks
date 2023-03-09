e33266fd1279d4b57c5adaaf088a55ddac30a04c
Colby <COLBY@COLBY.GOV>
2023-03-08 21:25:19 -0500

    implemented ChangeType

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,22 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [type, setType] = useState<QuestionType>("short_answer_question");
+    function flipType(): void {
+        if (type === "short_answer_question") {
+            setType("multiple_choice_question");
+        } else if (type === "multiple_choice_question") {
+            setType("short_answer_question");
+        }
+    }
+    return (
+        <span>
+            <Button onClick={flipType}>Change Type</Button>{" "}
+            {type === "short_answer_question" ? (
+                <span>Short Answer</span>
+            ) : (
+                <span>Multiple Choice</span>
+            )}
+        </span>
+    );
 }
