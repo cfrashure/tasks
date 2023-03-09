65af546467f581ec0b2a0e74d818a708da153c20
Colby <COLBY@COLBY.GOV>
2023-03-08 21:51:47 -0500

    implemented StartAttempt

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,39 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    const [attempt, setAttempt] = useState<number>(4);
+    const [progress, setProgress] = useState<boolean>(false);
+    function startQuiz(): void {
+        setProgress(true);
+        setAttempt(attempt - 1);
+    }
+    function stopQuiz(): void {
+        setProgress(false);
+    }
+    function increaseAttempt(): void {
+        setAttempt(attempt + 1);
+    }
+    return (
+        <>
+            <span>
+                <Button
+                    onClick={startQuiz}
+                    disabled={progress || attempt === 0}
+                >
+                    Start Quiz
+                </Button>
+            </span>
+            <span>
+                <Button onClick={stopQuiz} disabled={!progress}>
+                    Stop Quiz
+                </Button>
+            </span>
+            <span>
+                <Button onClick={increaseAttempt} disabled={progress}>
+                    Mulligan
+                </Button>
+            </span>
+            <span> {attempt}</span>
+        </>
+    );
 }
