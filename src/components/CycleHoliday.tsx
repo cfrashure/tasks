a17bb4333d92126fc83eeb3fe91567d761fcb4c6
Colby <COLBY@COLBY.GOV>
2023-03-08 22:33:03 -0500

    implemented cycleHolidays

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -2,5 +2,40 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    // Holidays: 😘, 🥚, 🇲🇽, 🎃, 🎄 (date)
+    // Holidays: 🎄, 🇲🇽, 🥚, 🎃, 😘(alphabetical)
+    const [holiday, setHoliday] = useState<string>("😘");
+    function alphaHoliday(): void {
+        const newEmoji = aHRec[holiday];
+        setHoliday(newEmoji);
+    }
+    function dateHoliday(): void {
+        const newEmoji = dHRec[holiday];
+        setHoliday(newEmoji);
+    }
+    const aHRec: Record<string, string> = {
+        "😘": "🎄",
+        "🥚": "🎃",
+        "🇲🇽": "🥚",
+        "🎃": "😘",
+        "🎄": "🇲🇽"
+    };
+    const dHRec: Record<string, string> = {
+        "😘": "🥚",
+        "🥚": "🇲🇽",
+        "🇲🇽": "🎃",
+        "🎃": "🎄",
+        "🎄": "😘"
+    };
+    return (
+        <>
+            <span>
+                <Button onClick={alphaHoliday}>Advance by Alphabet</Button>
+            </span>
+            <span>
+                <Button onClick={dateHoliday}>Advance by Year</Button>
+            </span>
+            <span>Holiday: {holiday}</span>
+        </>
+    );
 }
