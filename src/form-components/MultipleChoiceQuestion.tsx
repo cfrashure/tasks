80f8f80a2ab80c785d02a8422ba7dc6ef9aa9aa0
Colby <COLBY@COLBY.GOV>
2023-03-20 22:57:05 -0400

    implemented MultipleChoiceQuestion

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,25 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [choice, setChoice] = useState<string>(options[0]);
+    function updateChoice(event: React.ChangeEvent<HTMLSelectElement>) {
+        setChoice(event.target.value);
+    }
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="option-dropdown">
+                <Form.Label>Options</Form.Label>
+                <Form.Select value={choice} onChange={updateChoice}>
+                    {options.map((color: string) => (
+                        <option key={color} value={color}>
+                            {color}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            &nbsp;
+            <div>{choice === expectedAnswer ? "✔️" : "❌"}</div>
         </div>
     );
 }
